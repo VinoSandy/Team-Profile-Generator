15d38d8cbab5ad962e656057603f8ced06123628
Vinosandy <rvinothini@yahoo.co.in>
2024-02-24 23:20:38 +0000

    function to prompt Team Manager deatils added

--- index.js
+++ index.js
@@ -13,3 +13,51 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const team =[];
+
+function init(){
+
+    addManagerDetails();
+}
+
+function addManagerDetails(){
+
+    inquirer.prompt([
+                      { type: "input",
+                        name : "name",
+                        message: "Enter Team Manager's name:"
+                      },
+                      {
+                        type: "input",
+                        name: "id",
+                        message:"Enter Team Manager's Id:"
+
+                      },
+                      {
+                        type: "input",
+                        name: "email",
+                        message:"Enter Team Manager's Email-Id:"
+
+                      },
+                      {
+                        type: "input",
+                        name: "officeNumber",
+                        message:"Enter Team Manager's officeNumber:"
+
+                      },
+
+]).then(answer =>{
+    const manager = new Manager(
+                        answer.name,
+                         answer.id,
+                         answer.email,
+                          answer.officeNumber
+                            );
+      team.push(manager);                            
+      console.log(team[0]);
+})
+}
+
+
+
+init();
